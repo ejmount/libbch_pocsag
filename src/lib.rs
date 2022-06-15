fb6ce20b34860bb934f3dd540ccc6dcff5e3a965
Ewan Mount <ejmount@users.noreply.github.com>
2022-06-15 20:51:07 +0100

    Precomputing all possible syndrome values, with comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,32 @@
 #[cfg(test)]
 mod test;
 
-const SYNDROME_ERRORS: &[u32] = &[
-    0x3B4, 0x26E, 0x359, 0x076, 0x255, 0x0F0, 0x216, 0x365, 0x068, 0x25A, 0x343, 0x07B, 0x1E7,
-    0x129, 0x14E, 0x2C9, 0x0BE, 0x231, 0x0C2, 0x20F, 0x0DD, 0x1B4, 0x2B4, 0x334, 0x3F4, 0x394,
-    0x3A4, 0x3BC, 0x3B0, 0x3B6, 0x3B5,
-];
-
 const HIGHEST_BIT: u32 = u32::BITS - 1;
 
 const PAYLOAD_BITS: u32 = 21;
 const ECC_BITS: u32 = 10;
 const PARITY_BITS: u32 = 1;
 
+const ECC_MASK: u32 = low_bits_mask(ECC_BITS);
+const PAYLOAD_MASK: u32 = !low_bits_mask(ECC_BITS + PARITY_BITS);
+
+const SYNDROME_ERRORS: [(u32, u32, Option<u32>); count_bit_errors()] = enumerate_syndromes();
+
+// Create a mask to exctract the N lowest bits
 pub const fn low_bits_mask(n: u32) -> u32 {
     (1 << n) - 1
 }
 
-const ECC_MASK: u32 = low_bits_mask(ECC_BITS);
-const PAYLOAD_MASK: u32 = !low_bits_mask(ECC_BITS + PARITY_BITS);
-
-const fn bit_set(word: u32, n: u32) -> bool {
+// Is the given bit set
+const fn is_bit_set(word: u32, n: u32) -> bool {
     word & (1 << n) > 0
 }
 
-fn bits_ms(word: u32) -> impl Iterator<Item = bool> {
-    (0..u32::BITS).rev().map(move |n| bit_set(word, n))
+const fn high_bit(n: u32) -> u32 {
+    1 << n
 }
 
-fn from_bits(bits: impl Iterator<Item = bool>) -> u32 {
-    bits.fold(0, |t, n| (t << 1) | (n as u32))
-}
-
+// Calculate BCH checksum code, ignoring any existing ECC bits
 const fn get_bch_code(cw: u32) -> u32 {
     let mut local_cw = cw & PAYLOAD_MASK; // mask off BCH parity and even parity
 
@@ -39,7 +34,7 @@ const fn get_bch_code(cw: u32) -> u32 {
 
     // Calculate BCH bits
     while count < PAYLOAD_BITS {
-        if bit_set(local_cw, HIGHEST_BIT) {
+        if is_bit_set(local_cw, HIGHEST_BIT) {
             local_cw ^= 0xED_20_00_00;
         }
         local_cw <<= 1;
@@ -48,10 +43,56 @@ const fn get_bch_code(cw: u32) -> u32 {
     local_cw >> PAYLOAD_BITS
 }
 
-pub fn bch_encode(cw: u32) -> u32 {
-    let local_cw = (cw & PAYLOAD_MASK) | get_bch_code(cw);
+// Calculate "leftover" syndrome value from possibly-corrupt codeword
+// We do this by recalculating the BCH parity bits and XORing them against the received ones
+const fn calculate_syndrome(cw: u32) -> u32 {
+    ((bch_encode(cw) ^ cw) >> 1) & ECC_MASK
+}
 
+// How many possible error situations there are with any combination of 1 or 2 bit errors
+const fn count_bit_errors() -> usize {
+    let single_bits = u32::BITS as usize;
+    let double_bits = (((u32::BITS - 1) * u32::BITS) / 2) as usize;
+    double_bits + single_bits
+}
+
+// Produce a list of every possible syndrome value we might encounter
+const fn enumerate_syndromes() -> [(u32, u32, Option<u32>); count_bit_errors()] {
+    const EXAMPLE_WORLD: u32 = bch_encode(0x12340000);
+
+    let mut output = [(0, 0, None); count_bit_errors()];
+    let mut items = 0;
+
+    let mut bit = 0u32;
+    while bit < u32::BITS {
+        let error = high_bit(bit);
+        let corrupted = EXAMPLE_WORLD ^ error;
+        let syndrome = calculate_syndrome(corrupted);
+        output[items] = (syndrome, bit, None);
+        items += 1;
+        bit += 1;
+    }
+
+    let mut first_bit = 0u32;
+    while first_bit < u32::BITS {
+        let mut second_bit = 0;
+        while second_bit < first_bit {
+            let error = high_bit(first_bit) | high_bit(second_bit);
+            let corrupted = EXAMPLE_WORLD ^ error;
+            let syndrome = calculate_syndrome(corrupted);
+            output[items] = (syndrome, first_bit, Some(second_bit));
+            items += 1;
+            second_bit += 1;
+        }
+        first_bit += 1;
+    }
+
+    output
+}
+
+pub const fn bch_encode(cw: u32) -> u32 {
     // At this point local_cw contains a codeword with BCH but no parity
+    let local_cw = (cw & PAYLOAD_MASK) | get_bch_code(cw);
 
     // Calculate parity bit
     let parity = local_cw.count_ones();
@@ -60,69 +101,25 @@ pub fn bch_encode(cw: u32) -> u32 {
     local_cw | (parity % 2)
 }
 
-fn bit_decoder(syndrome: &mut u32) -> impl FnMut(bool) -> bool + '_ {
-    move |bit: bool| {
-        dbg!("    xbit:{}  synd:{:08X}", bit, &syndrome);
-
-        let output = if SYNDROME_ERRORS.iter().find(|&&s| s == *syndrome).is_some() {
-            // Syndrome matches an error in the MSB
-            // Correct that error and adjust the syndrome to account for it
-            *syndrome ^= 0x3B4;
-            dbg!("  E"); // indicate that an error was corrected in this bit
-            !bit
-        } else {
-            // no error
-            dbg!("   \n");
-            bit
-        };
-
-        // Handle Syndrome shift register feedback
-        if bit_set(*syndrome, ECC_BITS - PARITY_BITS) {
-            *syndrome <<= 1;
-            *syndrome ^= 0x769; // 0x769 = POCSAG generator polynomial -- x^10 + x^9 + x^8 + x^6 + x^5 + x^3 + 1
-        } else {
-            *syndrome <<= 1;
-        }
-        // mask off bits which fall off the end of the syndrome shift register
-        *syndrome &= low_bits_mask(ECC_BITS + PARITY_BITS);
-
-        output
-    }
-    // XXX Possible optimisation: Can we exit early if the syndrome is zero? (no more errors to correct)
-}
-
+// Attempt to repair the codeword, returning Err if recovery not possible
 pub fn bch_repair(cw: u32) -> Result<u32, ()> {
-    // calculate syndrome
-    // We do this by recalculating the BCH parity bits and XORing them against the received ones
-
-    // mask off data bits and parity, leaving the error syndrome in the LSB
-    let mut syndrome = ((bch_encode(cw) ^ cw) >> 1) & ECC_MASK;
+    // Get "leftover" syndrome
+    let syndrome = calculate_syndrome(cw);
 
     if syndrome == 0 {
         // Syndrome of zero indicates no repair required
         return Ok(cw);
     }
 
-    dbg!("cw:{:08X}  syndrome:{:08X}", cw, syndrome);
-
-    // --- Meggitt decoder ---
-    // Calculate repaired codeword
-    let result_bits = bits_ms(cw)
-        .take((PAYLOAD_BITS + ECC_BITS) as usize)
-        .map(bit_decoder(&mut syndrome));
-
-    let result = from_bits(result_bits) << PARITY_BITS;
-
-    dbg!(
-        "  orig:{cw:08X}  fixed:{result:08X}  {}",
-        if syndrome == 0 { "OK" } else { "ERR" }
-    );
-
-    // Check if error correction was successful
-    if syndrome == 0 {
-        Ok(result)
+    // Lookup the syndrome
+    if let Some((_, b, c)) = SYNDROME_ERRORS.iter().find(|(s, _, _)| *s == syndrome) {
+        // Retrieve indicces to repair, defaulting to no-op of cw^0
+        let firstbit = 1u32 << b;
+        let secondbit = c.map(|n| 1u32 << n).unwrap_or(0);
+        let corrected = cw ^ firstbit ^ secondbit;
+        Ok(bch_encode(corrected)) // Recalculate ECC bits based on the corrected payload
     } else {
-        // Syndrome nonzero at end indicates uncorrectable errors
+        // Did not recognise syndrome value to recover
         dbg!("nonzero syndrome at end");
         Err(())
     }
